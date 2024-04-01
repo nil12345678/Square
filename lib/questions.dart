ae4602f4fc5cc09399f4d127d80fd7c17f785df9
Niladri Bhattacharjee <bhattacharjeeniladri3@gmail.com>
2024-04-01 23:45:54 +0530

    Initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,454 @@
+import 'package:quiz_app/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    'What is the square of 1?',
+    [
+      '1',
+      '2',
+      '3',
+      '4',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 2?',
+    [
+      '4',
+      '6',
+      '8',
+      '10',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 3?',
+    [
+      '9',
+      '12',
+      '15',
+      '18',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 4?',
+    [
+      '16',
+      '24',
+      '32',
+      '40',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 5?',
+    [
+      '25',
+      '30',
+      '35',
+      '40',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 6?',
+    [
+      '36',
+      '42',
+      '48',
+      '54',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 7?',
+    [
+      '49',
+      '56',
+      '63',
+      '70',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 8?',
+    [
+      '64',
+      '72',
+      '80',
+      '88',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 9?',
+    [
+      '81',
+      '90',
+      '99',
+      '108',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 10?',
+    [
+      '100',
+      '110',
+      '120',
+      '130',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 11?',
+    [
+      '121',
+      '132',
+      '143',
+      '154',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 12?',
+    [
+      '144',
+      '156',
+      '168',
+      '180',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 13?',
+    [
+      '169',
+      '182',
+      '195',
+      '208',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 14?',
+    [
+      '196',
+      '210',
+      '224',
+      '238',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 15?',
+    [
+      '225',
+      '240',
+      '255',
+      '270',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 16?',
+    [
+      '256',
+      '272',
+      '288',
+      '304',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 17?',
+    [
+      '289',
+      '306',
+      '323',
+      '340',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 18?',
+    [
+      '324',
+      '342',
+      '360',
+      '378',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 19?',
+    [
+      '361',
+      '380',
+      '399',
+      '418',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 20?',
+    [
+      '400',
+      '420',
+      '440',
+      '460',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 21?',
+    [
+      '441',
+      '462',
+      '483',
+      '504',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 22?',
+    [
+      '484',
+      '506',
+      '528',
+      '550',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 23?',
+    [
+      '529',
+      '552',
+      '575',
+      '598',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 24?',
+    [
+      '576',
+      '600',
+      '624',
+      '648',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 25?',
+    [
+      '625',
+      '650',
+      '675',
+      '700',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 26?',
+    [
+      '676',
+      '702',
+      '728',
+      '754',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 27?',
+    [
+      '729',
+      '756',
+      '783',
+      '810',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 28?',
+    [
+      '784',
+      '812',
+      '840',
+      '868',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 29?',
+    [
+      '841',
+      '870',
+      '899',
+      '928',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 30?',
+    [
+      '900',
+      '930',
+      '960',
+      '990',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 31?',
+    [
+      '961',
+      '992',
+      '1023',
+      '1054',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 32?',
+    [
+      '1024',
+      '1056',
+      '1088',
+      '1120',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 33?',
+    [
+      '1089',
+      '1122',
+      '1155',
+      '1188',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 34?',
+    [
+      '1156',
+      '1190',
+      '1224',
+      '1258',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 35?',
+    [
+      '1225',
+      '1260',
+      '1295',
+      '1330',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 36?',
+    [
+      '1296',
+      '1332',
+      '1368',
+      '1404',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 37?',
+    [
+      '1369',
+      '1406',
+      '1443',
+      '1480',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 38?',
+    [
+      '1444',
+      '1482',
+      '1520',
+      '1558',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 39?',
+    [
+      '1521',
+      '1560',
+      '1599',
+      '1638',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 40?',
+    [
+      '1600',
+      '1640',
+      '1680',
+      '1720',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 41?',
+    [
+      '1681',
+      '1722',
+      '1763',
+      '1804',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 42?',
+    [
+      '1764',
+      '1806',
+      '1848',
+      '1890',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 43?',
+    [
+      '1849',
+      '1892',
+      '1935',
+      '1978',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 44?',
+    [
+      '1936',
+      '1980',
+      '2024',
+      '2068',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 45?',
+    [
+      '2025',
+      '2070',
+      '2115',
+      '2160',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 46?',
+    [
+      '2116',
+      '2162',
+      '2208',
+      '2254',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 47?',
+    [
+      '2209',
+      '2256',
+      '2303',
+      '2350',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 48?',
+    [
+      '2304',
+      '2352',
+      '2400',
+      '2448',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 49?',
+    [
+      '2401',
+      '2450',
+      '2499',
+      '2548',
+    ],
+  ),
+  QuizQuestion(
+    'What is the square of 50?',
+    [
+      '2500',
+      '2550',
+      '2600',
+      '2650',
+    ],
+  ),
+];
